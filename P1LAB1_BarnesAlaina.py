f979be17bcfda5e01f058cc61a04145fd174fa0e
alaina-edu <barnesa7458@student.faytechcc.edu>
2024-06-11 19:17:12 -0400

    Add files via upload

---
+++ P1LAB1_BarnesAlaina.py
@@ -0,0 +1,9 @@
+# Alaina Barnes
+ # 6/10/24
+ # P1LAB1
+ # Creating an input for first and last name
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello", first_name, last_name, "! Welcome to CTI-110")
